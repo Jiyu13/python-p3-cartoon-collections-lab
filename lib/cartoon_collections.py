3f92d26e68d00650e0f6cd669dcddbfb6b677ca6
Ziru Zou <jiyucoding@yahoo.com>
2023-02-19 08:06:44 -0500

    add solutions

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,18 +1,28 @@
-def roll_call_dwarves(dwarves):
-    for i in range(len(dwarves)):
-        print(f"{i+1}. {dwarves[i]}")
+CHEESES = ["camembert", "gouda", "cheddar"]
 
+def roll_call_dwarves(dwarf_list):
+    
+    i = 1
+    for name in dwarf_list:
+        print(f'{i}. {name}')
+        i += 1
 
 def summon_captain_planet(planeteer_calls):
-    return [f"{call.capitalize()}!" for call in planeteer_calls]
+    
+    return [f'{call.title()}!' for call in planeteer_calls]
 
-def long_planeteer_calls(short_words):
-    for i in range(len(short_words)):
-        if len(short_words[i]) > 4:
+def long_planeteer_calls(words):
+    
+    for word in words:
+        if len(word) > 4:
             return True
+    
     return False
 
-def find_the_cheese(snacks):
-    for snack in snacks:
-        if snack in ["cheddar", "gouda",  "camembert"]:
-            return snack
+def find_the_cheese(foods):
+    
+    for food in foods:
+        if food in CHEESES:
+            return food
+
+    return None
